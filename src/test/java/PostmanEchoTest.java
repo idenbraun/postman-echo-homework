8c2bfa6e101a18c34304af5b44e770e86c45b0f7
Денис <denis@MacBook-Pro-Denis.local>
2026-08-20 07:45:19 +0300

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,19 @@
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.jupiter.api.Test;
+
+class PostmanEchoTest {
+
+    @Test
+    void postBodyIsEchoedBack() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
